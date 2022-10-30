8eef5ec22d4853b4de9f18fa883a6d98a030558b
Gyanrugby <gygyrugby@gmail.com>
2022-10-30 09:57:14 +0100

    Login, register auth

--- src/utils/SetauthToken.js
+++ src/utils/SetauthToken.js
@@ -2,8 +2,12 @@ import axios from 'axios'
 const SetauthToken = token => {
     if(token){
         axios.defaults.headers.common['x-auth-token'] = token
+        axios.defaults.headers.common['Content-Type'] = 'application/json';
+        axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
     }else{
         delete axios.defaults.headers.common['x-auth-token']
+        delete axios.defaults.headers.common['Content-Type']
+        delete axios.defaults.headers.common['Access-Control-Allow-Origin']
     }
 }
 export default SetauthToken
